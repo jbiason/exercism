cf846156d00c082353541666b3b5df6a6a71655b
Julio Biason <julio.biason@pm.me>
2020-07-07 21:45:24 -0300

    Solution

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,42 @@
 (ns bob)
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn shout?
+  [x]
+  (let [filtered (filter #(Character/isLetter %) x)]
+    (if (empty? filtered)
+      false
+      (every? #(Character/isUpperCase %) filtered)
+      )
+    )
+  )
+
+(defn trim
+  [x]
+  (remove #(Character/isWhitespace %) x)
+  )
+
+(defn question?
+  [x]
+  (= (last (trim x)) \?)
+  )
+
+(defn silence?
+  [x]
+  (empty? (trim x))
+  )
+
+(defn forceful-question?
+  [x]
+  (and (shout? x) (question? x))
+  )
+
+(defn response-for 
+  [s]
+  (cond
+    (silence? s) "Fine. Be that way!" 
+    (forceful-question? s) "Calm down, I know what I'm doing!"
+    (shout? s) "Whoa, chill out!"
+    (question? s) "Sure."
+    :else "Whatever."
+    )
+  )
