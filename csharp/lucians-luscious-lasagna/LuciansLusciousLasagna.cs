75805c7b9501faa9ad9cad92905917a19a71a24c
Julio Biason <julio.biason@pm.me>
2021-09-29 18:29:51 -0300

    Exercism: Lucians Luscious Lasagna

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,18 @@
+class Lasagna
+{
+    public int ExpectedMinutesInOven() {
+        return 40;
+    }
+
+    public int RemainingMinutesInOven(int elapsed) {
+        return ExpectedMinutesInOven() - elapsed;
+    }
+
+    public int PreparationTimeInMinutes(int layers) {
+        return layers * 2;
+    }
+
+    public int ElapsedTimeInMinutes(int layers, int elapsed) {
+        return PreparationTimeInMinutes(layers) + elapsed;
+    }
+}
